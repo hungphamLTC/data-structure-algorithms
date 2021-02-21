e3661d6b2824c36d9f4093439d5c0ef073984893
hungphamLTC <pham.quang.thanh.hung@gmail.com>
2021-02-21 11:12:48 +0200

    stack pop method implementation

--- stack-queue/index.js
+++ stack-queue/index.js
@@ -5,7 +5,7 @@ class Node{
     }
 }
 
-class Stack{
+class Stack{ // implement stack with linked list
     constructor(){
         this.top = null;
         this.bottom = null;
@@ -28,6 +28,26 @@ class Stack{
         }
 
         this.length++;
+        return this;
+    }
+
+    pop(value){
+
+        if(!this.top){ // check if the stack is empty
+            return null;
+        }
+
+        if(this.top === this.bottom){ // check if the stack has only 1 item
+            this.bottom = null;
+        }
+
+        const hodlingPointer = this.top // this code is optional
+                                        //if we want to save the top value for later use
+                                        // we need to save it to a new reference otherwise it will be
+                                        // automatically erased from the memory
+                                        // due to garbage collected feature of javascript.
+        this.top = this.top.next;
+        this.length--;
         return this;
     }
 
